6dd3a5284e16a62c96a1d397bc95ade88d3fa809
Mark Obuobi <mark.eo@kraftkey.com>
2024-11-10 11:04:12 +0000

    Update ClearMessages.js

--- app/components/ClearMessages.js
+++ app/components/ClearMessages.js
@@ -3,7 +3,9 @@ import {useState} from "react";
 
 const ClearMessages = ({qty}) => {
     const [status, setStatus] = useState("");
+    const [clearing,setClearing] = useState(false)
     async function clearMessages(){
+        setClearing(true)
         setStatus('')
         const res = await fetch(process.env.NEXT_PUBLIC_API_URL + '/clear-messages')
         if (res.status === 200){
@@ -15,7 +17,7 @@ const ClearMessages = ({qty}) => {
 
     return (
         <>
-            <button onClick={clearMessages} className="bg-status-danger disabled:bg-status-danger/40 text-white disabled:text-white/20 p-3 rounded-md" disabled={qty === 0}>Clear Messages</button>
+            <button onClick={clearMessages} className="bg-status-danger disabled:bg-status-danger/40 text-white disabled:text-white/20 p-3 rounded-md" disabled={qty === 0 || clearing}>{clearing ? 'Clearing messages...' : 'Clear Messages'}</button>
             {status}
         </>
     )
